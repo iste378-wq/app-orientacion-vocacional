5cdddbf361be5645b8bf6bfdc266604a4c1dfa04
iste378-wq <luisgeovanny378@gmail.com>
2026-02-11 18:00:22 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🎓 SSistema de Orientación Vocacional")
+st.title("🎓 Sistema de Orientación Vocacional")
 st.markdown("Completa los datos y los 3 tests para obtener tu orientación.")
 
 # --- Inicialización del Estado ---
@@ -591,4 +591,5 @@ with tab5:
                 for key in st.session_state.keys():
                     del st.session_state[key]
                 st.rerun()
+
 
